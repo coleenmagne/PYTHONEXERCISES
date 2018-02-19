75876fc43ab1a87c1fd9225abb5923a0bf8458e4
coleenmagne <36612329+coleenmagne@users.noreply.github.com>
2018-02-19 13:05:19 +0800

    Add files via upload

---
+++ factorial.py
@@ -0,0 +1,7 @@
+factorial=input("Factorial of ")
+
+sum=1
+for x in range (1,factorial+1,+1):
+    sum=sum*x
+print ("factorial is",sum)
+    
---
+++ nintendowii.py
@@ -0,0 +1,6 @@
+budget=input("How much money do you have? ")
+print ("Your money is",budget)
+x=budget/50000
+print ("You can afford",x,"nintendo wii")
+z=50000-budget
+print ("You can add", z,"$")
